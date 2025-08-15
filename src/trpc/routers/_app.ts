ce622b96864749333c99be661be281ffd3c01f5a
nnsncl <nuniroland@gmail.com>
2025-08-15 22:11:01 +0200

    Setup trpc query sample

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 import { baseProcedure, createTRPCRouter } from "../init";
 
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
+  create: baseProcedure
     .input(
       z.object({
         text: z.string(),
@@ -10,10 +10,9 @@ export const appRouter = createTRPCRouter({
     )
     .query((opts) => {
       return {
-        greeting: `hello ${opts.input.text}`,
+        greeting: opts.input.text,
       };
     }),
 });
 
-// export type definition of API
 export type AppRouter = typeof appRouter;
--- src/trpc/server.tsx
+++ src/trpc/server.tsx
@@ -1,11 +1,15 @@
 import "server-only";
 
 import { cache } from "react";
-import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
+import {
+  createTRPCOptionsProxy,
+  type TRPCQueryOptions,
+} from "@trpc/tanstack-react-query";
 
 import { createTRPCContext } from "./init";
 import { makeQueryClient } from "./query-client";
 import { appRouter } from "./routers/_app";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 /**
  * IMPORTANT
@@ -18,3 +22,23 @@ export const trpc = createTRPCOptionsProxy({
   router: appRouter,
   queryClient: getQueryClient,
 });
+
+export function HydrateClient(props: { children: React.ReactNode }) {
+  const queryClient = getQueryClient();
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      {props.children}
+    </HydrationBoundary>
+  );
+}
+
+export function prefetch<T extends ReturnType<TRPCQueryOptions<any>>>(
+  queryOptions: T,
+) {
+  const queryClient = getQueryClient();
+  if (queryOptions.queryKey[1]?.type === "infinite") {
+    void queryClient.prefetchInfiniteQuery(queryOptions as any);
+  } else {
+    void queryClient.prefetchQuery(queryOptions);
+  }
+}
