318d78b4da519577b4443eb7375a19420ce4d4da
nnsncl <nuniroland@gmail.com>
2025-08-26 00:49:26 +0200

    Enhance user bubble UI

--- src/app/projects/[projectId]/_components/data/UserBubble.tsx
+++ src/app/projects/[projectId]/_components/data/UserBubble.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import styled from "styled-components";
 
 import { Badge } from "@usefui/components";
+import { format } from "date-fns";
 
 const Bubble = styled(Badge)`
   max-width: 80%;
@@ -12,14 +13,17 @@ const Bubble = styled(Badge)`
   padding: var(--measurement-medium-40) var(--measurement-medium-50) !important;
   border-radius: var(--measurement-medium-60) !important;
   border-top-right-radius: 0 !important;
+
+  font-weight: 500;
+  letter-spacing: calc((1.1618px / 2) * -1);
+  line-height: 1.1;
+  word-break: keep-all;
+
+  font-size: var(--fontsize-medium-20) !important;
 `;
 
 function UserBubble({ content }: { content: string }) {
-  return (
-    <Bubble variant="secondary">
-      <span className="fs-medium-20">{content}</span>
-    </Bubble>
-  );
+  return <Bubble variant="primary">{content}</Bubble>;
 }
 
 export default UserBubble;
