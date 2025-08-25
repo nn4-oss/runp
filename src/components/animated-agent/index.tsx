787b6b7b7ad411653b4cbd8b1b822b8170d9a4a6
nnsncl <nuniroland@gmail.com>
2025-08-26 00:50:05 +0200

    Add AnimatedAgent component

---
+++ src/components/animated-agent/index.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+import { Icon, PixelIcon } from "@usefui/icons";
+
+function AnimatedAgent() {
+  const [isHandsUp, setIsHandsUp] = React.useState<boolean>(true);
+
+  React.useEffect(() => {
+    const intervalId = setInterval(() => {
+      setIsHandsUp((prevIsHandsUp) => !prevIsHandsUp);
+    }, 500);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <Icon fill="var(--body-color)">
+      {isHandsUp ? <PixelIcon.HumanHandsup /> : <PixelIcon.HumanHandsdown />}
+    </Icon>
+  );
+}
+
+export default AnimatedAgent;
