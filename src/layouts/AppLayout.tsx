8e005c21412aca99b3e4602969bcd8abd0bfa781
nnsncl <nuniroland@gmail.com>
2025-08-31 20:56:34 +0200

    Remove duplicated layouts

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -2,8 +2,6 @@
 
 import React from "react";
 
-import EditorLayout from "./EditorLayout";
-
 import { Page } from "@usefui/components";
 import { Navigation } from "@/components";
 
@@ -13,15 +11,17 @@ function AppLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <EditorLayout>
-      <Navigation />
-      <Page.Wrapper
-        $navigations={1.1}
-        className="w-100 h-100 p-r-medium-30 p-b-medium-10 p-l-medium-30"
-      >
-        {children}
-      </Page.Wrapper>
-    </EditorLayout>
+    <Page>
+      <Page.Content>
+        <Navigation />
+        <Page.Wrapper
+          $navigations={1.1}
+          className="w-100 h-100 p-r-medium-30 p-b-medium-10 p-l-medium-30"
+        >
+          {children}
+        </Page.Wrapper>
+      </Page.Content>
+    </Page>
   );
 }
 
