02dd7d0c44be569f70c06af2c3bd27535f846dba
nnsncl <nuniroland@gmail.com>
2025-08-31 20:56:09 +0200

    Add ClerkProvider in layouts

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -2,6 +2,8 @@ import StyledComponentsRegistry from "@/lib/styles-registry";
 
 import { ClientProvider } from "@/providers";
 import { TRPCReactProvider } from "@/trpc/client";
+import { ClerkProvider } from "@clerk/nextjs";
+
 import { Toaster } from "sonner";
 
 export default function RootLayout({
@@ -11,28 +13,30 @@ export default function RootLayout({
 }>) {
   return (
     <StyledComponentsRegistry>
-      <TRPCReactProvider>
-        <ClientProvider>
-          <Toaster
-            toastOptions={{
-              style: {
-                borderRadius: "var(--measurement-medium-30)",
-                borderColor: "var(--font-color-alpha-10)",
-                background: "var(--body-color)",
-                color: "var(--font-color)",
-                fontSize: "var(--fontsize-medium-10)",
-              },
-            }}
-          />
+      <ClerkProvider>
+        <TRPCReactProvider>
+          <ClientProvider>
+            <Toaster
+              toastOptions={{
+                style: {
+                  borderRadius: "var(--measurement-medium-30)",
+                  borderColor: "var(--font-color-alpha-10)",
+                  background: "var(--body-color)",
+                  color: "var(--font-color)",
+                  fontSize: "var(--fontsize-medium-10)",
+                },
+              }}
+            />
 
-          <html lang="en" suppressHydrationWarning>
-            <body>
-              {children}
-              <div id="portal-container" />
-            </body>
-          </html>
-        </ClientProvider>
-      </TRPCReactProvider>
+            <html lang="en" suppressHydrationWarning>
+              <body>
+                {children}
+                <div id="portal-container" />
+              </body>
+            </html>
+          </ClientProvider>
+        </TRPCReactProvider>
+      </ClerkProvider>
     </StyledComponentsRegistry>
   );
 }
