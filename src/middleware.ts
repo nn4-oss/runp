c39127c3e479fc8b54d31878795d3cfbd740fe8d
nnsncl <nuniroland@gmail.com>
2025-09-01 16:03:53 +0200

    Update public routes to handle inngest and trpc routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,15 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
+  "/",
   "/sign-in(.*)",
   "/sign-up(.*)",
-  "/(.*)",
+  "/api/trpc(.*)",
+  "/api/inngest(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect();
-  }
+  if (!isPublicRoute(req)) await auth.protect();
 });
 
 export const config = {
