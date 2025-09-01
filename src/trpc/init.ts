f0239ae48fe5a587a2bc5d90f4aa1f2da8140667
nnsncl <nuniroland@gmail.com>
2025-09-01 16:12:58 +0200

    Add protectedProcedure

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,23 +1,36 @@
 import superjson from "superjson";
 
 import { cache } from "react";
-import { initTRPC } from "@trpc/server";
+import { auth } from "@clerk/nextjs/server";
+
+import { initTRPC, TRPCError } from "@trpc/server";
 
 export const createTRPCContext = cache(async () => {
   /**
-   * @see: https://trpc.io/docs/server/context
+   * The context will use the auth() helper from Clerk to access the user's Auth object.
+   * @see https://clerk.com/docs/references/nextjs/trpc
    */
-  return { userId: "user_123" };
+
+  return { auth: await auth() };
 });
 
-const t = initTRPC.create({
+const t = initTRPC.context<Context>().create({
   /**
    * @see https://trpc.io/docs/server/data-transformers
    */
   transformer: superjson,
 });
 
+const isAuthenticated = t.middleware(({ next, ctx }) => {
+  if (!ctx.auth.userId) throw new TRPCError({ code: "UNAUTHORIZED" });
+  return next({ ctx: { auth: ctx.auth } });
+});
+
 // Base router and procedure helpers
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+export const protectedProcedure = t.procedure.use(isAuthenticated);
+
+// Types
+export type Context = Awaited<ReturnType<typeof createTRPCContext>>;
