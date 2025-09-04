618dec25f5b524b2efbe93cb06b7b52e7ebdb472
nnsncl <nuniroland@gmail.com>
2025-09-04 21:51:48 +0200

    Enhance CredentialListActions UI

--- src/app/settings/api-keys/_components/CredentialsListActions.tsx
+++ src/app/settings/api-keys/_components/CredentialsListActions.tsx
@@ -4,12 +4,16 @@ import React from "react";
 
 import { Dialog } from "@usefui/components";
 import { CreateCredentialDialog } from "@/components";
+import { Icon, PixelIcon } from "@usefui/icons";
 
 function CredentialsListActions() {
   return (
     <div className="flex align-center g-medium-10">
       <Dialog.Root>
         <Dialog.Trigger variant="border" sizing="medium">
+          <Icon>
+            <PixelIcon.Plus />
+          </Icon>
           New API Key
         </Dialog.Trigger>
 
