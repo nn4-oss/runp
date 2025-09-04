024b9981c4e6f7c7207b79a3496bffadf54db4a7
nnsncl <nuniroland@gmail.com>
2025-09-04 15:51:38 +0200

    Prepare settings pages

---
+++ src/app/settings/api-keys/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { HydrateClient, prefetch, trpc } from "@/trpc/server";
+import CredentialsList from "./_components/CredentialsList";
+
+async function Page() {
+  prefetch(trpc.credentials.getMany.queryOptions());
+
+  return (
+    <HydrateClient>
+      <CredentialsList />
+    </HydrateClient>
+  );
+}
+
+export default Page;
