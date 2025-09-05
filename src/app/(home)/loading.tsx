548f1bacc11d7af7053908802ed1229b27eb0e81
nnsncl <nuniroland@gmail.com>
2025-09-05 13:55:35 +0200

    Center loading pages spinners

--- src/app/(home)/loading.tsx
+++ src/app/(home)/loading.tsx
@@ -5,7 +5,7 @@ import { Spinner } from "@/components";
 
 function Loading() {
   return (
-    <section className="flex h-100 align-center justify-center">
+    <section className="flex h-100 w-100 align-center justify-center">
       <Spinner />
     </section>
   );
