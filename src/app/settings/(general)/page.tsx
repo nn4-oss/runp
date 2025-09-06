9989d1f8186ad3d5ee863b5de4c022cca41af08d
nnsncl <nuniroland@gmail.com>
2025-09-06 16:30:14 +0200

    prepare settings workflows

--- src/app/settings/(general)/page.tsx
+++ src/app/settings/(general)/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { HydrateClient } from "@/trpc/server";
 
 async function Page() {
-  return <HydrateClient>Settings</HydrateClient>;
+  return <HydrateClient>General</HydrateClient>;
 }
 
 export default Page;
