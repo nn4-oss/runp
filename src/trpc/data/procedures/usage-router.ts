128ebc4f21645819579befe8e59d1381ba646b62
nnsncl <nuniroland@gmail.com>
2025-09-07 23:02:11 +0200

    Add getMetadata procedure to usageRouter

--- src/trpc/data/procedures/usage-router.ts
+++ src/trpc/data/procedures/usage-router.ts
@@ -1,11 +1,29 @@
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { getUsageStatus } from "@/services/usage-services";
 
+import { POINTS_PER_SCOPE } from "@/utils/scope-features";
+
 export const usageRouter = createTRPCRouter({
   status: protectedProcedure.query(async () => {
     try {
       const result = await getUsageStatus();
       return result;
+    } catch (error) {
+      return null;
+    }
+  }),
+
+  getMetadata: protectedProcedure.query(async ({ ctx }) => {
+    try {
+      const result = await getUsageStatus();
+
+      const scopePoints = POINTS_PER_SCOPE[ctx.user.scope as "FREE" | "PRO"];
+      const consumedPoints = result?.consumedPoints ?? 0;
+      const usageInPercent = consumedPoints / scopePoints;
+
+      return {
+        percentage: usageInPercent,
+      };
     } catch (error) {
       return null;
     }
