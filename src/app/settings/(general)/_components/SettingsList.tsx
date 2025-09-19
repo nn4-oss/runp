7a4758da097f1c4b7a33b16c7977f7dc6407d9ed
nnsncl <nuniroland@gmail.com>
2025-09-19 21:27:55 +0200

    Add reset config feature

--- src/app/settings/(general)/_components/SettingsList.tsx
+++ src/app/settings/(general)/_components/SettingsList.tsx
@@ -1,20 +1,61 @@
 "use client";
 
-import React from "react";
+import React, { useCallback } from "react";
+
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery } from "@tanstack/react-query";
+
 import GeneralSettings from "./GeneralSettings";
 
-import { FixedHeader } from "@/components";
+import { FixedHeader, Spinner } from "@/components";
 import { Button, Page } from "@usefui/components";
 
+import { toast } from "sonner";
+
 function SettingsList() {
+  const trpc = useTRPC();
+
+  const { data: user, isPending } = useQuery(trpc.user.get.queryOptions());
+  const { data: config } = useQuery(
+    trpc.configuration.getLatestVersion.queryOptions(),
+  );
+
+  const isFreeScope = user?.scope === "FREE";
+  const hasDefaultConfig =
+    config?.at(0)?.diagrams === false && config?.at(0)?.additionalPrompt === "";
+  const disableReset = isPending || isFreeScope || hasDefaultConfig;
+
+  const updateConfig = useMutation(
+    trpc.configuration.update.mutationOptions({
+      onSuccess: () => {
+        window && window.location.reload();
+      },
+      onError: (error) => toast.error(error.message),
+    }),
+  );
+
+  const handleReset = useCallback(async () => {
+    return await updateConfig.mutateAsync({
+      id: String(config?.at(0)?.id),
+      diagrams: false,
+      additionalPrompt: "",
+    });
+  }, []);
+
   return (
     <Page.Content className="w-100 h-100" scrollbar>
       <FixedHeader className="grid">
         <div className="flex justify-between align-center p-y-medium-60 p-x-medium-60">
           <p className="fs-medium-20">Application Settings</p>
 
-          <Button variant="border" sizing="medium" disabled>
+          <Button
+            variant="border"
+            sizing="medium"
+            disabled={disableReset || updateConfig.isPending}
+            onClick={handleReset}
+          >
             Reset default
+            {updateConfig.isPending && <Spinner />}
           </Button>
         </div>
       </FixedHeader>
