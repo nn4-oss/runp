418698a29d3b4ce5a1b0a317b8de26a97f9bcc66
nnsncl <nuniroland@gmail.com>
2025-09-24 20:39:13 +0200

    Enhance ProjectHeader UI

--- src/app/projects/[projectId]/_components/navigations/ProjectHeader.tsx
+++ src/app/projects/[projectId]/_components/navigations/ProjectHeader.tsx
@@ -3,8 +3,19 @@
 import React from "react";
 import styled from "styled-components";
 
-import { Field, Page, Tooltip, Button } from "@usefui/components";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
+import {
+  Field,
+  Page,
+  Tooltip,
+  Button,
+  DropdownMenu,
+  Badge,
+} from "@usefui/components";
 import { Icon, PixelIcon, WebIcon } from "@usefui/icons";
+import { Spinner } from "@/components";
 
 import type { Fragment } from "generated/prisma";
 import type { ViewProps } from "../../_types";
@@ -29,55 +40,119 @@ function ProjectsHeader({
   setSandboxKey,
   setCurrentView,
 }: ProjectHeaderProps) {
+  const trpc = useTRPC();
+
+  const { data: user, isPending: isUserPending } = useQuery(
+    trpc.user.get.queryOptions(),
+  );
+  const { data: config, isPending } = useQuery(
+    trpc.configuration.getLatestVersion.queryOptions(),
+  );
+
   const handleRefresh = () => setSandboxKey((k) => k + 1);
   const handleNewTab = () => {
     if (!fragment?.sandboxUrl) return;
     window.open(fragment?.sandboxUrl, "_blank", "noopener,noreferrer");
   };
-  const handleViewChange = () => {
-    const next = currentView === "code" ? "preview" : "code";
-    if (next === "preview" && !fragment?.sandboxUrl) return;
+  const handleViewChange = (next: ViewProps) => {
     setCurrentView(next);
   };
 
-  const switchViewLabel = currentView === "code" ? "Preview" : "Code";
-  const switchViewTooltip =
-    !fragment?.sandboxUrl && currentView === "code"
-      ? "Preview unavailable"
-      : `Show ${switchViewLabel}`;
-
+  const isProMode = user?.scope === "PRO";
+  const hasConfig = !isPending && config?.id;
+  const hasDiagramsEnabled = isProMode && Boolean(hasConfig && config.diagrams);
   const disableIframeInteractions =
-    !fragment?.sandboxUrl || currentView === "code";
+    !fragment?.sandboxUrl || ["code", "diagram"].includes(currentView);
 
   return (
     <StyledMenu className="w-100 flex g-medium-10 align-center justify-between">
-      <Tooltip content={switchViewTooltip}>
-        <Button
-          disabled={!fragment?.sandboxUrl}
-          variant="border"
-          sizing="small"
-          aria-label={`Show ${switchViewLabel}`}
-          onClick={handleViewChange}
-        >
-          <span className="flex align-center justify-center p-y-small-30">
-            <Icon>
-              {currentView === "code" && <PixelIcon.Eye />}
-              {currentView === "preview" && <WebIcon.DataObject />}
-            </Icon>
-          </span>
-        </Button>
-      </Tooltip>
+      <DropdownMenu.Root>
+        <DropdownMenu>
+          <Tooltip content="Views">
+            <DropdownMenu.Trigger
+              variant="border"
+              sizing="small"
+              disabled={isPending || isUserPending}
+            >
+              {isPending || isUserPending ? (
+                <Spinner />
+              ) : (
+                <span className="flex align-center justify-center p-y-small-30">
+                  <Icon>
+                    <WebIcon.More />
+                  </Icon>
+                </span>
+              )}
+            </DropdownMenu.Trigger>
+          </Tooltip>
 
-      <Field
-        variant="secondary"
-        sizing="small"
-        readOnly
-        className="fs-medium-10"
-        value={fragment?.sandboxUrl ?? "/"}
-        style={{ textAlign: "center", width: "100%" }}
-      />
+          <DropdownMenu.Content>
+            <DropdownMenu.Item
+              onClick={() => handleViewChange("preview")}
+              className="flex align-center g-medium-30"
+            >
+              <span className="flex align-center justify-center p-y-small-30">
+                <Icon>
+                  <PixelIcon.Eye />
+                </Icon>
+              </span>
+              Preview
+              {currentView === "preview" && (
+                <span className="justify-end w-100 flex">
+                  <Icon>
+                    <PixelIcon.ChevronRight />
+                  </Icon>
+                </span>
+              )}
+            </DropdownMenu.Item>
+            <DropdownMenu.Item
+              onClick={() => handleViewChange("code")}
+              className="flex align-center g-medium-30 w-100"
+            >
+              <span className="flex align-center justify-center p-y-small-30">
+                <Icon>
+                  <WebIcon.Code />
+                </Icon>
+              </span>
+              Code
+              {currentView === "code" && (
+                <span className="justify-end flex w-100">
+                  <Icon>
+                    <PixelIcon.ChevronRight />
+                  </Icon>
+                </span>
+              )}
+            </DropdownMenu.Item>
+            <DropdownMenu.Item
+              onClick={() => hasDiagramsEnabled && handleViewChange("diagram")}
+              className="flex align-center g-medium-30"
+              disabled={!hasDiagramsEnabled}
+            >
+              <span className="flex align-center justify-center p-y-small-30">
+                <Icon>
+                  <WebIcon.Decisions />
+                </Icon>
+              </span>
+              Diagram
+              <span className="justify-end w-100 flex align-center">
+                {!isProMode && (
+                  <Badge variant="secondary">
+                    <span className="fs-small-50">PRO</span>
+                  </Badge>
+                )}
 
-      <div className="flex align-center g-medium-10">
+                {currentView === "diagram" && (
+                  <Icon>
+                    <PixelIcon.ChevronRight />
+                  </Icon>
+                )}
+              </span>
+            </DropdownMenu.Item>
+          </DropdownMenu.Content>
+        </DropdownMenu>
+      </DropdownMenu.Root>
+      <span className="fs-medium-10 opacity-default-10">/</span>
+      <div className="flex align-center g-medium-10 w-100">
         <Tooltip content="Open in new tab">
           <Button
             disabled={disableIframeInteractions}
@@ -109,6 +184,14 @@ function ProjectsHeader({
             </span>
           </Button>
         </Tooltip>
+        <Field
+          variant="secondary"
+          sizing="small"
+          readOnly
+          className="fs-medium-10"
+          value={fragment?.sandboxUrl ?? "/"}
+          style={{ width: "100%" }}
+        />
       </div>
     </StyledMenu>
   );
